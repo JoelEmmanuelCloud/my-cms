7b8c5ddd5a25f02c2e28a2f9432b83b61f8af24a
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-02 18:19:35 +0100

    feat: setup joi for user validation

---
+++ src/validators/user-validator.ts
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+
+const signupSchema = Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    birthDate: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    confirmPassword: Joi.string()
+        .valid(Joi.ref('password'))
+        .required()
+        .messages({
+            'any.only': 'Confirm password must match the password',
+        }),
+}).prefs({ abortEarly: false });
+
+const signInSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+});
+
+export { signupSchema, signInSchema };
