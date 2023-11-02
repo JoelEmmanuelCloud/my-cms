aeb3721c000213165f8f15aaef7d264740309042
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-02 18:16:39 +0100

    feat: create user route

---
+++ src/routes/user-route.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+const router = express.Router();
+import { createJWT } from '../utils/jwt';
+import { signUpUser, signInUser } from '../controllers/user-controller';
+import { signupSchema, signInSchema } from '../validators/user-validator';
+import { StatusCodes } from 'http-status-codes';
+
+router.post('/sign-up', async (req, res) => {
+    const newUserSignupData = req.body;
+
+    try {
+        const { error, value } = signupSchema.validate(newUserSignupData);
+
+        if (error) {
+            res.status(StatusCodes.BAD_REQUEST).json({ errors: error.details });
+            return;
+        }
+        const user = await signUpUser(value);
+
+        const userWithoutPassword = { ...user.toObject() };
+        delete userWithoutPassword.password;
+
+        res.status(StatusCodes.CREATED).json(userWithoutPassword);
+    } catch (error) {
+        const errorMessage = (error as Error).message;
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: errorMessage,
+        });
+    }
+});
+
+router.post('/sign-in', async (req, res) => {
+    const signInData = req.body;
+
+    try {
+        const { error, value } = signInSchema.validate(signInData);
+
+        if (error) {
+            res.status(StatusCodes.BAD_REQUEST).json({ errors: error.details });
+            return;
+        }
+
+        const user = await signInUser(value);
+
+        const token = createJWT({ payload: { _id: user._id } });
+
+        res.status(StatusCodes.OK).json({
+            token,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+        });
+    } catch (error) {
+        const errorMessage = (error as Error).message;
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: errorMessage,
+        });
+    }
+});
+
+export default router;
