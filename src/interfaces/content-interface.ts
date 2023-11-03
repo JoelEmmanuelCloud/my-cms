e55ccb3fecd0f762609cc8cd7c8c062e7184c744
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-03 03:16:36 +0100

    refactor: update content interface

--- src/interfaces/content-interface.ts
+++ src/interfaces/content-interface.ts
@@ -1,10 +1,16 @@
 import { Document } from 'mongoose';
 
-interface IContent extends Document {
+export interface Content {
+    _id: string;
+    title: string;
+    content: string;
+    published: boolean;
+    userId: string;
+}
+
+export interface IContentDocument extends Document {
   title: string;
   content: string;
   published: boolean;
   userId: string;
 }
-
-export default IContent
