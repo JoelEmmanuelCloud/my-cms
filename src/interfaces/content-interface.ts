ec83a2073d787c7629fcbd680721d7adbd1f9a65
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-03 11:07:44 +0100

    feat: add more fields to content types

--- src/interfaces/content-interface.ts
+++ src/interfaces/content-interface.ts
@@ -1,16 +1,26 @@
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export interface Content {
     _id: string;
     title: string;
-    content: string;
+    body: string;
     published: boolean;
     userId: string;
+    slug: string;
+    categories: string[];
+    tags: string[];
+    status: 'draft' | 'published';
+    author: Types.ObjectId;
 }
 
 export interface IContentDocument extends Document {
-  title: string;
-  content: string;
-  published: boolean;
-  userId: string;
+    title: string;
+    body: string;
+    published: boolean;
+    userId: string;
+    slug: string;
+    categories: string[];
+    tags: string[];
+    status: 'draft' | 'published';
+    author: Types.ObjectId; 
 }
