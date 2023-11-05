1ef0e0a3acf73bb3a41c23dc407b10e24386b98f
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-05 10:03:46 +0100

    feat: create a model for blacklistedTokens

---
+++ src/models/blacklistedTokens.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Model, Schema } from 'mongoose';
+
+interface IBlacklistedToken extends Document {
+    userId: string;
+    token: string;
+    createdAt: Date;
+}
+
+const BlacklistedTokenSchema: Schema = new Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: '1d'
+    },
+});
+
+const BlacklistedTokenModel: Model<IBlacklistedToken> = mongoose.model<IBlacklistedToken>('BlacklistedToken', BlacklistedTokenSchema);
+
+export default BlacklistedTokenModel;
