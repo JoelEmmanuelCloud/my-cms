6d9c6aec4d40dd0c26899f935728728ad3075999
Joel Emmanuel <ejoel0035@gmail.com>
2023-11-08 19:37:16 +0100

    feat: setup update user controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,29 @@
+import {
+    BadRequestError,
+    NotFoundError,
+} from '../errors/api-errors';
+import UserModel from '../models/users';
+import { IUser, IUserDocument } from '../interfaces/user-interface';
+
+async function updateUserDetails(userId: string, updateData: IUser): Promise<IUserDocument> {
+    try {
+        const user = await UserModel.findById(userId);
+
+        if (!user) {
+            throw new NotFoundError('User not found');
+        }
+
+        Object.keys(updateData).forEach((key) => {
+            // @ts-ignore - Ignore for TypeScript: dynamically setting properties
+            user[key] = updateData[key];
+        });
+
+        await user.save();
+
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export { updateUserDetails };
